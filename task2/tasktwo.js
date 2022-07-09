63ebdb138b505e436f3d5c8538892f7cd5104282
shehandesilva <shehandesilva04@gmail.com>
2022-07-09 10:05:23 +0530

    task 2 complete

---
+++ task2/tasktwo.js
@@ -0,0 +1,25 @@
+var movies=["batman","thor","superman","spiderman","avengers"];
+
+if (movies[3].charAt(0)=="i"){
+    console.log('Yay');
+}
+
+else{
+    console.log('Nay');
+}
+
+
+movies=[];
+
+movies[3]='ironman';
+
+if (movies[3].charAt(0)=="i"){
+    console.log('Yay');
+}
+
+else{
+    console.log('Nay');
+}
+
+
+// console.log(movies);
