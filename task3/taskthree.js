7b2c32566849a76f8889381c799703aaf0b07938
shehandesilva <shehandesilva04@gmail.com>
2022-07-09 10:12:00 +0530

    task 3 complete

---
+++ task3/taskthree.js
@@ -0,0 +1,15 @@
+var car={
+    model:'BMW',
+    color:'balck',
+    price:'80'
+};
+
+if (car.price>50){
+    console.log('Expensive');
+}
+
+else {
+    console.log('Affordable');
+}
+
+console.log(car.color);
